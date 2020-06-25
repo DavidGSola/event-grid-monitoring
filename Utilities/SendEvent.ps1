c9d7171f59ed483990877c2d3cbecf3e3539e321
David Gonzalez Sola <David.Gonzalez.Sola@unit4.com>
2020-06-25 21:25:11 +0200

    Allow customize number of events (even infinite) in SendEvent.ps1 script. Allow custom event file in SendEvent.ps1 script.

--- Utilities/SendEvent.ps1
+++ Utilities/SendEvent.ps1
@@ -1,11 +1,52 @@
-param ($topicKey, $topicUri)
+<#
 
-$RequestParams = @{
-    ContentType = 'application/cloudevents+json; charset=utf-8' 
-    Body = Get-Content -Raw -Path 'event.json'
-    Method = 'Post'
-    URI = $topicUri
-    Headers = @{'aeg-sas-key'=$topicKey}
+.SYNOPSIS
+Script to send events to an Event Grid Topic
+
+.DESCRIPTION
+The script sends events read from a json file to a specific Event Grid Topic.
+Mandatory and optional fields:
+	- TopicKey (Mandatory): Event Grid Topic Key.
+	- TopicUri (Mandatory): Event Grid Topic Uri.
+	- EventFile (Optional): Path to a json file with the event content. Defaulted to 'event.json'.
+	- NumberEvents (Optional): Number of events to send. Use -1 for an infinite run.
+
+.EXAMPLE
+
+To send 10 events using a specific json file:
+./SendEvent.ps1 -topicKey MyBeautifulTopicKey -topicUri https://my-amazing-topic.com/events -eventFile otherEvent.json -numberEvents 10
+
+To send infinite events using a specific json file:
+./SendEvent.ps1 -topicKey MyBeautifulTopicKey -topicUri https://my-amazing-topic.com/events -numberEvents -1
+
+#>
+
+Param(
+[Parameter(Mandatory=$True)]  
+[string]$topicKey,    
+  
+[Parameter(Mandatory=$True)]    
+[string]$topicUri,
+
+[string]$eventFile='event.json',
+
+[int]$numberEvents
+)    
+
+For ($i=1; $i -le $numberEvents -Or $numberEvents -eq -1; $i++) {
+	$rnd = Get-Random -Maximum 2000
+	Start-Sleep -Milliseconds $rnd
+	
+	$eventContent = Get-Content -Raw -Path $eventFile | ConvertFrom-Json
+	$eventContent.time = Get-Date -Format o
+	
+	$RequestParams = @{
+		ContentType = 'application/cloudevents+json; charset=utf-8' 
+		Body = $eventContent | ConvertTo-Json
+		Method = 'Post'
+		URI = $topicUri
+		Headers = @{'aeg-sas-key'=$topicKey}
+	}
+	
+	Invoke-RestMethod @RequestParams
 }
-
-Invoke-RestMethod @RequestParams
